68219cb92977215b8d29f0ae48b2b9b4805f0ca9
nuklearfiziks <andi@nuklearfiziks.com>
2018-10-02 01:34:42 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,30 @@
+E-TMA.bot
+-------
+
+The kinder, simpler cousin of LIGMA.bot. :smile:
+
+# Setup instructions
+
+## 1. Create bot account somewhere
+
+I like [botsin.space](https://botsin.space) personally
+
+## 2. Create an API token
+
+This is a multi stage step!
+
+i. Go to "Edit Profile" once logged into your bot account
+ii. Click the "Developer" link in the sidebar
+iii. Click "New Application"
+iv. Give it a name, leave everything else the same; click Done
+v. Go back to your bot after creating it
+vi. Copy the newly generated big-ass string in the "Your access token" field
+
+Note, be careful with this token as anyone with it can act as that user!
+
+### 3. Click dis button hurrr vvvv
+
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/nuklearfiziks/etma&ENV=ACCESS_TOKEN&ENV=API_URL&ENV=USERNAME&ENV=POST_EVERY_X_MINUTES)
+
+
+### 4. Fill out the form and you're donezo
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * etma-bot
+ * 2018 Andi N. Fiziks (@nuklearfiziks@bofa.lol)
+ *
+ * Simple Markov-chains-based Mastodon bot
+ */
+
+const Mastodon = require('mastodon-api');
+const Markov = require('markov-strings');
+
+const {
+	ACCESS_TOKEN,
+	API_URL = 'https://botsin.space/api/v1/',
+	USERNAME = '@Pyretta@girlcock.club',
+	POST_EVERY_X_MINUTES = 30,
+	MAX_LENGTH = 400
+} = process.env;
+
+if (!ACCESS_TOKEN) {
+	throw new Error('Please supply an access token via env var ACCESS_TOKEN!');
+}
+
+const {
+	getUserToots,
+	getUserId
+} = require('./lib/generate-corpus');
+
+const M = new Mastodon({
+	access_token: ACCESS_TOKEN,
+	api_url: API_URL
+});
+
+(async () => {
+	try {
+		const userId = await getUserId(M, USERNAME);
+		const toots = await getUserToots(M, userId);
+		const marky = new Markov(toots, {
+			maxLength: MAX_LENGTH,
+			minWords: 10,
+			minScore: 25,
+			filter: result => {
+				return result.string.endsWith('.'); // I want my tweets to end with a dot.
+			}
+		});
+		await marky.buildCorpus();
+		setInterval(async () => {
+			try {
+				const status = await marky.generateSentence();
+				await M.post('statuses', {status});
+			} catch (error) {
+				console.error(error);
+			}
+		}, POST_EVERY_X_MINUTES * 60 * 1000);
+	} catch (error) {
+		console.error(error.statusCode);
+	}
+})();
